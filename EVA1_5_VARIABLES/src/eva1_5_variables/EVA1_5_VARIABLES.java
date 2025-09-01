88f8633a89d19084405bf018c8c0597cb697279f
Alejandro7641 <L25550420@chihuahua2.tecnm.mx>
2025-09-01 10:14:07 -0600

    Add files via upload

---
+++ EVA1_5_VARIABLES/src/eva1_5_variables/EVA1_5_VARIABLES.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva1_5_variables;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_5_VARIABLES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //declarar las variables 
+        //tipo de dato  nombre del identificador
+        String nombre;
+        int edad;
+        double salario;
+        //inicializacion
+        //identificador = valor
+        //= --> operador de asignacion
+        nombre="Alejandro Nuñez";
+        edad=18;
+        salario=1.1;
+        System.out.println(nombre);
+        System.out.println(edad);
+        System.out.println(salario);
+    }
+    
+}
